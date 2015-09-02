54e854e1093ae937a5f3cf53ad3a5ae0a76f3eba
GwangWoon <wwka123@naver.com>
2015-09-02 13:46:30 +0900

    Lec1 0902
    문자열 예제

---
+++ GW0902/GW0902/GW0902.py
@@ -0,0 +1,30 @@
+﻿#print("Hello World")
+#print("i have %d cats" %6)
+#print("한글")
+
+#salary = input("plz enter your salary : ")
+#bonus = input("plz enter your bonus : ")
+
+#paycheck = salary + bonus
+#print(paycheck)
+#print(type(paycheck))
+#paycheck = float(salary) + float(bonus)
+#print(paycheck)
+#print(type(paycheck))
+#print("="*10)
+#name ="greenjoa"
+#print(name[0])
+#print(name[-1])
+#print(name[-2])
+#print(name[0:3])
+#a="pithon"
+#a=a[:1]+'y'+a[2:]
+#print(a)
+a="i eat %10s apples." % "five"
+print(a)
+a="i eat %-10s apples." % "five"
+print(a)
+a="Error is %d%%" %98
+print(a)
+message = input("입력 : ")
+print(message.lower())
