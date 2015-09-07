733a54a07e5fb7393cd6acce648c2bac4a8d6193
GwangWoon <wwka123@naver.com>
2015-09-07 13:51:19 +0900

    Lec2 0907

--- GW0907/GW0907/GW0907.py
+++ GW0907/GW0907/GW0907.py
@@ -41,12 +41,43 @@
 #for steps in guest : #step = guest[0],guest[1],...
 #    print(steps)
 #print('------------------')
+#------------------------------------------------------
 
-scores = [85,42,11,23,56,54,44,65,78]
-scores.sort()
-print(scores)
-scores.reverse()
-print(scores)
 
-top5=scores[0:5] #상위 5개 뽑기
-print(top5)
+
+
+#scores = [85,42,11,23,56,54,44,65,78]
+#scores.sort()
+#print(scores)
+#scores.reverse()
+#print(scores)
+
+#top5=scores[0:5] #상위 5개 뽑기
+#print(top5)
+#------------------------------------------------------
+
+#data = ['a','b','c',['abcd','efg']]
+
+#for steps in data:
+#    if isinstance(steps, list) : #이중 리스트 들어갈경우(steps가 list라면)
+#        for step in steps : #steps에서 다시 돌림
+#            print(step)
+#    else :
+#        print(steps)
+#------------------------------------------------------
+
+#scores = [85,42,11,23,56,54,23,65,78]
+#num = scores.pop(2) #2번째요소를 반환하고 삭제
+#print(num)
+#print(scores)
+
+#num = scores.count(23) #스코어에서 23의 갯수 반환
+#print(num)
+
+#scores.extend([50,60]) #리스트에 추가
+#print(scores)
+#------------------------------------------------------
+
+t1=()
+t2=(1,)
+t3=(1,2,3)
