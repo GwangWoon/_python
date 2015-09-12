00d150db6bcad8a204869d74f8ef73f68118733e
GwangWoon <wwka123@naver.com>
2015-09-13 01:44:42 +0900

    과제1 거북이 움직이기

---
+++ 201111258 bugi/201111258 bugi/_201111258_bugi.py
@@ -0,0 +1,64 @@
+import turtle
+import math
+import time
+import random
+
+def addTurtle():
+    global num
+    bugi.append('bugi')
+    bugi[num] = turtle.Turtle()
+    bugi[num].shape('turtle')
+    bugi[num].goto(0,0)
+    bugi[num].color(random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
+    bugi[num].penup()
+    num = num + 1
+
+def speedUp() :
+    global speed
+    if speed < 10 :
+        speed = speed+1
+
+def speedDown() :
+    global speed
+    if speed > 0 :
+        speed = speed-1
+
+def moveTurtle(radius):
+    while True:
+        global speed
+        screen.onkey(speedUp, "Up")
+        screen.onkey(speedDown, "Down")
+        screen.onkey(addTurtle, "space")
+        screen.listen()
+        for turtle in screen.turtles():
+            if turtle.distance(0,0) > radius:
+                turtle.forward(-5)
+                turtle.setheading(random.randrange(0,360,10))
+            else :
+                turtle.forward(speed)
+
+
+screen = turtle.Screen()
+screen.title('201111258 GoGwangWoon')
+screen.colormode(255)
+
+num = 0
+speed = 3
+nSides = 5
+bugi = []
+
+addTurtle()
+
+bugi[0].goto(0,-200)
+bugi[0].begin_fill()
+bugi[0].circle(200)
+bugi[0].end_fill()
+time.sleep(1)
+
+bugi[0].color('red')
+bugi[0].circle(200)
+bugi[0].goto(0,0)
+time.sleep(1)
+        
+
+moveTurtle(200)
